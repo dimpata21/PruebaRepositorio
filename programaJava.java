df1a1d06819a6deeaeaa1e605ae8649c0e213d25
dimpata21 <84255275+dimpata21@users.noreply.github.com>
2021-12-04 13:04:43 -0500

    Create programaJava.java

---
+++ programaJava.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author dimpa
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+        //A través del objeto entrada podemos guardar los datos que queramos
+        
+        int numero;
+        
+        System.out.println("Digite un numero: ");
+        numero = entrada.nextInt();
+        
+        System.out.println("El numero es: " + numero);
+        
+    }
+    
+}
